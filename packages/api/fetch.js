0c9f0498cf487769ad82d32dfd8b24ca9ca02943
moor <812446905@qq.com>
2026-03-01 22:42:15 +0800

    chore: remove diagnostic scripts and temporary utilities

--- packages/api/fetch.js
+++
@@ -1,4 +0,0 @@
-fetch('https://dynosync-api.dynosync-dev.workers.dev/public/vehicles/7c112185-5447-4872-acb0-9c6bf93f6f58')
-    .then(res => res.text())
-    .then(console.log)
-    .catch(console.error);
